3b88e3ee34d9cec9905bda0f2de9e9822efad360
NikiTesla <krechetov.n@outlook.com>
2023-05-06 21:19:22 +0300

    edited default values for db settings

--- friends/settings.py
+++ friends/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'friends'),
         'USER': os.environ.get('DB_USER','postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD','pass'),
-        'HOST': os.environ.get("DB_HOST", "db"),
-        'PORT': int(os.environ.get("DB_PORT", "5432")),
+        'HOST': os.environ.get("DB_HOST", "127.0.0.1"),
+        'PORT': int(os.environ.get("DB_PORT", "5434")),
     }
 }
 
